eb5051fa41948ce94066a4fc11999d1903695b7d
洪子胜 <3440253650@qq.com>
2026-07-20 12:26:21 +0800

    feat：增加数据库迁移工具

--- complex-events-backend/app/models/__init__.py
+++ complex-events-backend/app/models/__init__.py
@@ -1,1 +1,9 @@
 from .Result import Result
+from .database_models import Base, WorkerType, Worker, WorkerTeam, SelectedWorker, \
+    EquipmentCategory, EquipmentType, EquipmentInstance, SelectedEquipment, \
+    Material, MaintenanceTool, ProcessTemplate, \
+    WorkOrder, WorkOrderTask, WorkOrderTaskWorker, \
+    MaintenancePlan, SchedulePlan, ScheduleTask, \
+    User, UserWx, Document, UploadedFile, \
+    GraphNode, GraphNodeArchive, GraphRelation, GraphRelationArchive, \
+    TaskOperationLog
